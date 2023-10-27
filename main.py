33e2e0e25515c89c666f579922d8a010a5cd95c8
Maya A <m590a.roon55@gmail.com>
2023-10-27 16:00:53 -0600

    managed_error corrected

--- main.py
+++ main.py
@@ -107,10 +107,10 @@ def manage_error(error):
         window_alert("The server is not responding")
     elif "CommandNotFoundException" in error:
         # Install the module
-        ruta_script = os.path.join(os.getcwd(), ast.literal_eval(os.environ.get("SCRIPT_INSTALL_MODULE")))
+        ruta_script = os.path.join(os.getcwd(), os.environ.get("SCRIPT_INSTALL_MODULE"))
         subprocess.run(["powershell", "-File", ruta_script])
     else:
-        window_alert(f"An error occurred: {error}")
+        window_alert(f"An error occurred during installing MSTools: {error}")
 
 
 def window_alert(message):
