a9bcd82ac4708597847208e8e5f3f2ebf4b89e36
Maya A <m590a.roon55@gmail.com>
2023-12-20 10:17:50 -0600

    Create add_hardware
    Script to add new hardware to Milestone server from a CSV file

---
+++ add_hardware/add_hardware.ps1
@@ -0,0 +1,10 @@
+<#
+# Script to add a list of cameras to a recording server in Milestone from a CSV file.
+  Input: IP address of the server, CSV file with the list of cameras to add, recording server name.
+  Output: Message of success
+  Requirements: PowerShell, MilestonePSTools module v23.2.3
+
+  Author: Maya Aguirre
+  Date: 2023-12-14
+#>
+
