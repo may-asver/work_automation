b4a06e5c63449761f39cf746d29dd61bdb7d3594
Maya A. <m590a.roon55@gmail.com>
2024-04-19 15:34:26 -0600

    Create milestone_operations
    Create milestone_operations script

---
+++ all_functions/milestone_operations.py
@@ -0,0 +1,8 @@
+"""
+    Script to show a menu and select an operation to do in Milestone Server.
+    Input: operation, server IP address, CSV file
+
+    Author: Maya Aguirre
+    Date: 2024-04-19
+"""
+
