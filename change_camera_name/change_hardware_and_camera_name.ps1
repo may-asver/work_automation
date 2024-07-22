c41bb59f94e2c9f0dcc4afe5953f58104afa291a
Maya A. <m590a.roon55@gmail.com>
2024-07-22 15:20:40 -0600

    Update change_hardware_and_camera_name
    Set action preference to handle errors, describe the function Select-File, change the line to get file path and validation, change csv file encoding to Default, create new variable to count lines, validate parameters null, change the output or errors in catch, add a line to make pause before closing the window

--- change_camera_name/change_hardware_and_camera_name.ps1
+++ change_camera_name/change_hardware_and_camera_name.ps1
@@ -1,4 +1,5 @@
 <# Script to change the hardware and camera name of cameras from a CSV file.
+#
 #  Input: CSV file
 #  Output: Message of success
 #  Requirements: PowerShell, MilestonePSTools module v23.2.3
@@ -6,7 +7,10 @@
 #  Date: 2024-04-19
 #>
 
+# Set action preference to handle the errors in catch
+$ErrorActionPreference = "Stop"
 
+# Function that opens a file dialog to select a CSV file
 function Select-File
 {
     # Load the Windows Forms assembly
@@ -27,43 +31,51 @@ function Select-File
     return $null
 }
 
+# Main processes to change hardware name and camera name
 try
 {
     # Import the CSV file
     Write-Host "Select the CSV file"
-    $path = Invoke-Command -ScriptBlock ${function:Select-File}
-    if ($null -eq $path)
+    $path = Select-File
+    if ($null -eq $path) # Validate if value is null
     {
-        Write-Host "No file selected"
-        exit
+        Write-Error "No se ha seleccionado un archivo"
     }
-    $csv = Import-Csv -Path $path -Delimiter "," -Encoding "UTF8"
+    $csv = Import-Csv -Path $path -Delimiter "," -Encoding "Default"
+
+    # Get the file number line where is running
+    $number_line = 1
 
     # Change the name of each camera from the CSV file
     foreach ($row in $csv)
     {
-        # Connect to the server
-        Connect-ManagementServer $row.ServerIP
-        $hardware = Get-Hardware -Name $row.HardwareName
-        # Validate that hardware exists
-        if ($null -eq $hardware)
+        # Validate that parameters are not null
+        if ('' -eq $row.HardwareName -or '' -eq $row.CameraName -or '' -eq $row.NewHardwareName -or '' -eq $row.NewCameraName -or '' -eq $row.ServerIP)
         {
-            Write-Host "Camera $row.HardwareName not found"
-            exit
+            Write-Error "Alguno o todos los valores de la linea $number_line en el archivo estan vacios"
         }
-        # Change camera and hardware name
-        Write-Host "Changing name of camera $hardware.HardwareName" # Displays a message to user
-        $camera = Get-VmsCamera -Hardware $hardware # Get camera using hardware parameter
-        Set-VmsHardware -Hardware $hardware -Name $row.NewHardwareName # Change Hardware name
-        Set-VmsCamera -Camera $camera -Name $row.NewCameraName # Change camera name
-        # Disconnect from the server
-        Disconnect-ManagementServer
+        else
+        {
+            # Connect to the server
+            Connect-ManagementServer $row.ServerIP
+            $hardware = Get-Hardware -Name $row.HardwareName
+            # Change camera and hardware name
+            Write-Host "Changing name of camera $($hardware.HardwareName)" # Displays a message to user
+            $camera = Get-VmsCamera -Hardware $hardware # Get camera using hardware parameter
+            Set-VmsHardware -Hardware $hardware -Name $row.NewHardwareName # Change Hardware name
+            Set-VmsCamera -Camera $camera -Name $row.NewCameraName # Change camera name
+            # Disconnect from the server
+            Disconnect-ManagementServer
+        }
+        # Sum 1 to number line
+        $number_line ++
     }
     Write-Host "Done"
-
 }
 catch
 {
-    Write-Error "Error:" $error
+    Write-Output "Ocurrio un error: $_"
 }
 
+# Set pause to read errors before close the main window
+Read-Host -Prompt "Presione Enter para cerrar la ventana"
