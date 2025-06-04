bdbbd6755ac2c2990c7f44231160bf8076d89338
Maya A. <m590a.roon55@gmail.com>
2025-06-04 15:44:41 -0600

    Update get_list_cameras.ps1
    Change requirements, error action preference, validate if version needed is installed

--- get_cameras/get_list_cameras.ps1
+++ get_cameras/get_list_cameras.ps1
@@ -1,15 +1,19 @@
 <#
     Script to get all cameras on Milestone Recording servers.
 
-    Requirements: MilestonePSTools 23.2.3
+    Requirements: MilestonePSTools 22.3.0
     Ouput: Error message, Success message, verbose message.
 
     Author: Maya Aguirre
     Date: 06/08/2024
 #>
 
+# GLOBAL VARIABLES
+$module_name = "MilestonePSTools"
+
 # Set action preference to handle the errors in catch
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = "Continue"
+
 
 
 # Function to select the folder to save the file
@@ -23,13 +27,21 @@ function Select-FolderDialog
     return $folderBrowser.SelectedPath
 }
 
+
+
 # Main process to get the list of cameras and save it on csv file
 try
 {
-    # Import module
-    Import-Module -Name 'MilestonePSTools'
-    Write-Output "Modulo importado"
+    # Validate if the version needed is installed
+    $versions = Get-InstalledModule -Name $module_name -AllVersions
+    if ($versions.Version[0] -notlike "22.*")
+    {
+        throw "La version del modulo no es compatible."
+    }
 
+    # Import minimum version installed
+    Import-Module $module_name -RequiredVersion $versions.Version[0]
+    
     # Select folder to save the file
     $folder_path = Select-FolderDialog
 
@@ -46,16 +58,19 @@ try
     # Get list of cameras of each server
     foreach ($server in $servers)
     {
-        # Create file and name
-        $fileName = "$date-$($server.Nombre)"
-        $csvFile = Join-Path -Path $folder_path -ChildPath "$fileName.csv"
+        $serverName = $server.Nombre
+        $serverIP = $server.IP
 
+        # Create file and name
+        $fileName = "$date-$($serverName)"
+        $csvFile = Join-Path -Path $folder_path -ChildPath "$fileName.csv"
+         
         # Connect to server
-        Write-Output "$($server.Nombre) $($server.IP)"
-        Connect-ManagementServer $server.IP
+        Write-Output "$($serverName) $($serverIP)" # Message to know which server is
+        Connect-ManagementServer $serverIP
 
         # Advise to user in which server is working
-        Write-Output "Obteniendo la lista del servidor $($server.Nombre)"
+        Write-Output "Obteniendo la lista del servidor $($serverName)"
         
         # Get list of cameras
         Get-VmsCameraReport -IncludePlainTextPasswords | Export-csv -Path $csvFile -Encoding "Default"
@@ -68,7 +83,11 @@ try
 }
 catch
 {
+    # Manage errors
     Write-Output "Occurrio un error: $_"
+    
+    # Close session
+    Disconnect-ManagementServer
 }
 
 # Set pause to read errors before close the main window
